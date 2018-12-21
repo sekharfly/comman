b6c600a0faa73ea94f0346ac32f74bd1c2a1bf66
sekharfly <sekharfly@gmail.com>
2018-12-21 18:51:51 +0530

    commited

--- advertiser/src/test/java/advertiser/Mangement/advertiser/AdvertiserApplicationTests.java
+++ advertiser/src/test/java/advertiser/Mangement/advertiser/AdvertiserApplicationTests.java
@@ -1,7 +1,12 @@
 package advertiser.Mangement.advertiser;
 
+import static org.junit.Assert.assertEquals;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Optional;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -9,12 +14,21 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.gson.Gson;
 
 import advertiser.Mangement.advertiser.Controller.AdvertiserController;
 import advertiser.Mangement.advertiser.model.Advertiser;
@@ -24,16 +38,21 @@ import advertiser.Mangement.advertiser.repo.AdvertiserRepo;
 @WebMvcTest(value = AdvertiserController.class)
 public class AdvertiserApplicationTests {
 
+	@LocalServerPort
+	String randomServerPort;
+	
+	@Autowired
+	Gson gson;
+
 	@Autowired
 	private MockMvc mockMvc;
 
 	@MockBean
 	private AdvertiserRepo studentService;
 
-	
 	@Test
 	public void findById() throws Exception {
-		Advertiser advertiser = new Advertiser(1l,"vinay","vinay r","100");
+		Advertiser advertiser = new Advertiser(1l, "vinay", "vinay r", "100");
 		Optional<Advertiser> of = Optional.of(advertiser);
 		Mockito.<Optional<Advertiser>>when(studentService.findById(Mockito.any())).thenReturn(of);
 		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/advertiser/getAdvertiser/1")
@@ -47,4 +66,48 @@ public class AdvertiserApplicationTests {
 		JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
 	}
 
+/*	@Test
+	public void newAdvertiser() throws URISyntaxException {
+		System.out.println(randomServerPort);
+		RestTemplate restTemplate = new RestTemplate();
+		String valueOf = String.valueOf(randomServerPort);
+		final String baseUrl = "http://localhost:" + valueOf + "/advertiser/newAdvertiser";
+		URI uri = new URI(baseUrl);
+		Advertiser employee = new Advertiser(1, "Adam", "Gilly", "10");
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "application/json");
+		HttpEntity<Advertiser> request = new HttpEntity<>(employee, headers);
+
+		try {
+			restTemplate.postForEntity(uri, request, String.class);
+			Assert.fail();
+		} catch (Exception ex) {
+			// Verify bad request and missing header
+			// Assert.assertEquals(400, ex.getRawStatusCode());
+			// Assert.assertEquals(true, ex.getResponseBodyAsString().contains("Missing
+			// request header"));
+			ex.printStackTrace();
+
+		}*/
+		
+		
+	@Test
+	public void newAdvertiser() throws Exception {
+		Advertiser advertiser = new Advertiser(1l, "vinay", "vinay r", "100");
+		// studentService.addCourse to respond back with mockCourse
+		Mockito.when(studentService.save(Mockito.any())).thenReturn(advertiser);
+		// Send course as body to /students/Student1/courses
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/advertiser/newAdvertiser")
+				.accept(MediaType.APPLICATION_JSON).content(gson.toJson(advertiser))
+				.contentType(MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+		MockHttpServletResponse response = result.getResponse();
+
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
+	}	
 }
+
+
